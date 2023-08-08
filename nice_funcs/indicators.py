ec51860901e6b2ec2beb91146905aa6f3f198616
zanyev <relima222@gmail.com>
2023-08-07 21:11:51 -0300

    fix:

--- nice_funcs/indicators.py
+++ nice_funcs/indicators.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-btc = pd.read_excel(r'C:\Users\Renato\Desktop\desafio_quant\desafio_quant_2023\assets\1d\BTCUSDT.xlsx')
-
 def EWMA(s:pd.Series,window:int)->pd.Series:
-    ewma = s.ewm(span=10, adjust=False).mean()
+    """
+    Implementação da média movel exponencial
+    atributos:
+    s: series de preços 
+    window: tamanho da janela movel
+    return: serie de precos aplicada a media exponencial
+    """
+    ewma = s.ewm(span=window, adjust=False).mean()
+    ewma.iloc[:window] = np.nan
     return ewma
-def MACD():
-    pass
 
-print(EWMA(btc['Close'],10))
+
