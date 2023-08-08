ab64e4ab680f85fb9c64f18adf90d0db8cfe887c
zanyev <relima222@gmail.com>
2023-08-07 23:10:23 -0300

    add: ddpg_v1 actor,critit e buffer class

--- nice_funcs/indicators.py
+++ nice_funcs/indicators.py
@@ -27,3 +27,21 @@ def Normalize(s:pd.Series)->pd.Series:
     return normalized
 
 
+def CreateRandomPrtf(n):   
+    """Gera um array de tamanho n+1, onde suas entradas somam 1.
+    Método de sorteio em um simplexo. Possui distribuição uniforme.
+    int -> array"""
+
+    aleatorio = [0]
+    for _ in range(0, n):
+        aleatorioi = np.random.rand()   # Gero números entre [0, 1) uniformemente
+        aleatorio.append(aleatorioi)    # Os adiciono a lista.
+
+    aleatorio.sort()                    # Ordeno-a
+    aleatorio.append(1)
+
+    pesos = np.array(aleatorio[1:]) - np.array(aleatorio[:-1])
+    
+    return pesos
+
+
