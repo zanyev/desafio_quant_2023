a79cbaa8e1f88754836c0e7a77c9b0b9d657522b
zanyev <relima222@gmail.com>
2023-08-08 22:54:37 -0300

    add

--- nice_funcs/indicators.py
+++ nice_funcs/indicators.py
@@ -46,14 +46,14 @@ def CreateRandomPrtf(n):
 
 
 
-def MACD(s:pd.Series)-> [pd.Series, pd.Series, pd.Series]:
+def MACD(s:pd.Series, media_longa = 26, media_curta = 12)-> [pd.Series, pd.Series, pd.Series]:
     """Recebendo a série de fechamento da ação, retorna 
     MACD = EWMA_12 - EWMA_26
     Signal = EWMA_9
     Histograma = Signal - MACD"""
 
-    MACD = EWMA(s, 12) - EWMA(s, 26)
-    MACD_s = EWMA(s, 9)
+    MACD = EWMA(s, media_longa) - EWMA(s, media_curta)
+    MACD_s = EWMA(MACD, 9)
     MACD_h = MACD_s - MACD
 
     return MACD, MACD_s, MACD_h
