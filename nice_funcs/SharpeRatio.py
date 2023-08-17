520708e42850dc88bef4a947a86a4622467f272d
Pedro Paulo <epedropaulodsm@gmail.com>
2023-08-16 22:47:26 -0300

    Add.: Adicionando a func do sharpe.

--- nice_funcs/SharpeRatio.py
+++ nice_funcs/SharpeRatio.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-def SharpeRatio(retorno:pd.Series, window=252)->pd.Series:
+def SharpeRatio(retorno:pd.Series, window=365)->pd.Series:
     """Recebendo a série que contém os retornos da carteira, retorna o índice Sharpe dela.
     sharpe = (Retorno - RiskFree)/std(Retorno)"""
-
-    Retorno_movel = retorno.rolling(window=window, min_periods=1)   # Faz a janela móvel dos retornos
-    std_movel = Retorno_movel.std()                                 # Desvio padrão das janelas
     
-    riskfree = 0
+    std_retorno = retorno.std()
 
-    indice_sharpe = (Retorno_movel - riskfree)/std_movel
+    # Vamos calcular o retorno do bond de 10 anos dos EUA. 08/08/2018 até 08/08/2023
+    path='../assets/United States 10-Year Bond Yield Historical Data.csv'
+    close_price_free=pd.read_csv(path)['Close']
+    retorno_riskfree = (close_price_free[1:] - close_price_free[:-1]) / close_price_free[:-1]
+
+    indice_sharpe = (np.mean(retorno) - np.mean(retorno_riskfree))/std_retorno
 
     return indice_sharpe
